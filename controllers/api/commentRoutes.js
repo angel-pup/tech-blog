1a77a23e0eae25003d33112ff61288ef02a1f3a1
Spencer Stewart <r.spencer.stewart@gmail.com>
2023-05-18 22:51:16 -0500

    updated API with new paths that allow comment manipulation

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -7,17 +7,17 @@ router.post('/', (req, res) => {
     .catch(err => res.status(400).json({ message: err.message }));
 });
 
-// TODO: Possibly needed if single comment fetch is needed
-// router.get('/:id', (req, res) => {
-//   Comment.findByPk(req.params.id)
-//     .then(comment => {
-//       if (!comment) {
-//         return res.status(404).json({ message: 'comment not found'});
-//       }
-//       res.status(200).json(comment);
-//     })
-//     .catch(err => res.status(400).json({ message: err.message }));
-// });
+router.get('/:blog_id', (req, res) => {
+  Comment.findOne({ where: { blog_id: req.params.blog_id } })
+    .then(comment => {
+      if (!comment) {
+        return res.status(404).json({ message: 'Comment not found' });
+      }
+      res.status(200).json(comment);
+    })
+    .catch(err => res.status(400).json({ message: err.message }));
+});
+
 
 // TODO: Possibly need the following code in the event udpating/deleting comments is needed
 // router.put('/:id', (req, res) => {
@@ -31,15 +31,15 @@ router.post('/', (req, res) => {
 //     .catch(err => res.status(400).json({ message: err.message }));
 // });
 
-// router.delete('/:id', (req, res) => {
-//   Comment.destroy({ where: { id: req.params.id } })
-//     .then(deleted => {
-//       if (!deleted) {
-//         return res.status(404).json({ message: 'comment not found'});
-//       }
-//       res.status(200).json({ message: 'comment deleted' });
-//     })
-//     .catch(err => res.status(400).json({ message: err.message }));
-// });
+router.delete('/:id', (req, res) => {
+  Comment.destroy({ where: { id: req.params.id } })
+    .then(deleted => {
+      if (!deleted) {
+        return res.status(404).json({ message: 'comment not found'});
+      }
+      res.status(200).json({ message: 'comment deleted' });
+    })
+    .catch(err => res.status(400).json({ message: err.message }));
+});
 
 module.exports = router;
