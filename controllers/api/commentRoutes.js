56383c04f1f27c5c4cefd3b9510c807757e0b43c
Spencer Stewart <r.spencer.stewart@gmail.com>
2023-05-19 00:15:08 -0500

    removed intentially created security vulns and added auth middleware to U/D paths

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -7,16 +7,16 @@ router.post('/', (req, res) => {
     .catch(err => res.status(400).json({ message: err.message }));
 });
 
-router.get('/:blog_id', (req, res) => {
-  Comment.findAll ({ where: { blog_id: req.params.blog_id } })
-    .then(comment => {
-      if (!comment) {
-        return res.status(404).json({ message: 'Comment not found' });
-      }
-      res.status(200).json(comment);
-    })
-    .catch(err => res.status(400).json({ message: err.message }));
-});
+// router.get('/:blog_id', (req, res) => {
+//   Comment.findAll ({ where: { blog_id: req.params.blog_id } })
+//     .then(comment => {
+//       if (!comment) {
+//         return res.status(404).json({ message: 'Comment not found' });
+//       }
+//       res.status(200).json(comment);
+//     })
+//     .catch(err => res.status(400).json({ message: err.message }));
+// });
 
 
 // TODO: Possibly need the following code in the event udpating/deleting comments is needed
@@ -31,15 +31,15 @@ router.get('/:blog_id', (req, res) => {
 //     .catch(err => res.status(400).json({ message: err.message }));
 // });
 
-router.delete('/:id', (req, res) => {
-  Comment.destroy({ where: { id: req.params.id } })
-    .then(deleted => {
-      if (!deleted) {
-        return res.status(404).json({ message: 'comment not found'});
-      }
-      res.status(200).json({ message: 'comment deleted' });
-    })
-    .catch(err => res.status(400).json({ message: err.message }));
-});
+// router.delete('/:id', (req, res) => {
+//   Comment.destroy({ where: { id: req.params.id } })
+//     .then(deleted => {
+//       if (!deleted) {
+//         return res.status(404).json({ message: 'comment not found'});
+//       }
+//       res.status(200).json({ message: 'comment deleted' });
+//     })
+//     .catch(err => res.status(400).json({ message: err.message }));
+// });
 
 module.exports = router;
